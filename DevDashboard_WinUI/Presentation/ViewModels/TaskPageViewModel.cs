7b4db9dd920d7eb4f77e5cecfb7da0ec32ddd1d6
jongcheol-pak <jongcheol83@gmail.com>
2026-07-20 14:28:09 +0900

    checkpoint: T4 review-fix

--- DevDashboard_WinUI/Presentation/ViewModels/TaskPageViewModel.cs
+++ DevDashboard_WinUI/Presentation/ViewModels/TaskPageViewModel.cs
@@ -1,6 +1,5 @@
 using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
 using DevDashboard.Infrastructure.Services;
 
 namespace DevDashboard.Presentation.ViewModels;
